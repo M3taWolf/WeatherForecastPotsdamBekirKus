3fd5522b2533cc8e906dad4ef5777e6de3a591e2
bekirkus <bekir.kus@hotmail.de>
2023-01-25 13:11:59 +0100

    Initial Commit

---
+++ WeatherForecastPotsdamBekirKus/WeatherForecastPotsdamBekirKusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastPotsdamBekirKusApp.swift
+//  WeatherForecastPotsdamBekirKus
+//
+//  Created by Bekir Kus on 25.01.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastPotsdamBekirKusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
